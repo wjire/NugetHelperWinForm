a3dbff0e7505e195d3a9118354f0ac1da7b44ac6
Administrator <Administrator@ADMINISTRATOR>
2018-05-12 18:28:02 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace NugetHelperWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(12, 191);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(79, 30);
+            this.btnConfirm.TabIndex = 0;
+            this.btnConfirm.Text = "确认";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(216, 191);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 30);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(10, 130);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(281, 55);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(9, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "最新稳定版";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblVersion.Location = new System.Drawing.Point(85, 46);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(65, 12);
+            this.lblVersion.TabIndex = 6;
+            this.lblVersion.Text = "最新稳定版";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(9, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "即将上传版";
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtVersion.Location = new System.Drawing.Point(85, 73);
+            this.txtVersion.Multiline = true;
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(65, 21);
+            this.txtVersion.TabIndex = 8;
+            this.txtVersion.Text = "1.0.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(9, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "程序集名称";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblName.Location = new System.Drawing.Point(85, 18);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(65, 12);
+            this.lblName.TabIndex = 10;
+            this.lblName.Text = "程序集名称";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(8, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(207, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "下面写描述,千万不要使用回车换行";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 233);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtVersion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Name = "Form1";
+            this.Text = "NugetHelperWinForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NugetHelperWinForm
+{
+    public partial class Form1 : Form
+    {
+        //AssemblyInfo.cs文件的绝对路径
+        private readonly string filePath;
+
+        //项目文件夹的绝对路径,含文件夹名
+        private readonly string projectFileName;
+
+        //项目名称
+        private readonly string targetName;
+
+        //小工具所在的文件夹的绝对路径,不含文件夹名
+        private readonly string toolPath = AppConfigSetting.ToolPath;
+
+        //nuget 站点,如: http://www.mynuget.com
+        private readonly string nugetUrl = AppConfigSetting.NugetUrl;
+
+        //api 接口
+        private readonly string apiUri = AppConfigSetting.ApiUri;
+
+        //pwd
+        private readonly string pwd = AppConfigSetting.Pwd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+
+            projectFileName = args[0];
+            targetName = args[1];
+            filePath = args[2] + @"Properties\AssemblyInfo.cs";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = nugetUrl + apiUri + targetName;
+            lblName.Text = targetName;
+            txtDescription.Text = url;
+            var version = HttpGet(url).Trim('"');
+            if (string.IsNullOrWhiteSpace(version))
+            {
+                lblVersion.Text = "第一次上传该项目";
+                txtVersion.Text = "1.0.0";
+            }
+            else
+            {
+                lblVersion.Text = version;
+                txtVersion.Text = UpdateVersion(lblVersion.Text);
+            }
+        }
+
+        /// <summary>
+        /// 确认
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var line in File.ReadLines(filePath))
+            {
+                CheckLine(sb, line);
+            }
+            File.WriteAllText(filePath, sb.ToString());
+            ProcessCmd();
+            this.Close();
+            this.Dispose();
+        }
+
+        /// <summary>
+        /// 取消
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+
+
+        /// <summary>
+        /// 修改程序集版本号,描述,作者等数据
+        /// </summary>
+        /// <param name="sb"></param>
+        /// <param name="line"></param>
+        void CheckLine(StringBuilder sb, string line)
+        {
+            if (line.Contains("AssemblyDescription"))
+            {
+                sb.AppendLine("[assembly: AssemblyDescription(\"" + txtDescription.Text.Replace('\"', ' ').Replace('\\', ' ').Trim() + "\")]");
+            }
+            else if (line.Contains("AssemblyCompany"))
+            {
+                //当前windows登录用户名
+                sb.AppendLine("[assembly: AssemblyCompany(\"" + Environment.UserName + "\")]");
+            }
+            else if (line.Contains("AssemblyCopyright"))
+            {
+                sb.AppendLine("[assembly: AssemblyCopyright(\"" + "Copyright © " + Environment.UserName + " " + DateTime.Now.ToString("yyyy-MM-dd") + "\")]");
+            }
+            else if (line.Contains("AssemblyVersion") && !line.Contains("*"))
+            {
+                sb.AppendLine("[assembly: AssemblyVersion(\"" + txtVersion.Text + "\")]");
+            }
+            else
+            {
+                sb.AppendLine(line);
+            }
+        }
+
+
+        /// <summary>
+        /// 执行 windows 批处理文件
+        /// </summary>
+        private void ProcessCmd()
+        {
+            Process proc = new Process();
+            try
+            {
+                //设置批处理文件的路径
+                proc.StartInfo.FileName = toolPath + @"\NugetHelper.bat";
+
+                //设置批处理文件的参数
+                string[] args = new string[]
+                {
+                    projectFileName,
+                    targetName,
+                    toolPath,
+                    nugetUrl + "/nuget",
+                    pwd
+                };
+
+                //在 cmd 命令提示符下运行程序,多个参数以 空格 隔开
+                proc.StartInfo.Arguments = args.Aggregate((a, s) => a += " " + s);
+
+                proc.StartInfo.CreateNoWindow = false;
+                proc.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception Occurred :{ex.Message},{ex.StackTrace}");
+            }
+        }
+
+        /// <summary>
+        /// 请求api,获取当前使用的最高版本号
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        string HttpGet(string url)
+        {
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// 计算最新版本号 比如: 1.2.3 => 1.2.4
+        /// </summary>
+        /// <param name="nowVersion"></param>
+        /// <returns></returns>
+        string UpdateVersion(string nowVersion)
+        {
+            var nowVersionStrArray = nowVersion.Split('.');// 1 2 3
+            var nowLastNum = nowVersionStrArray.Last();// 3
+            var newLastNum = Convert.ToInt32(nowLastNum) + 1;// 4
+            nowVersionStrArray[2] = newLastNum.ToString();// 1 2 4
+            return nowVersionStrArray.Aggregate((a, s) => a += "." + s);//1.2.4
+        }
+    }
+}
